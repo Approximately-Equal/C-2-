4ef966e8534399122e4ccef61c3d1fd914367faa
Approximately_Equal <95661722+Approximately-Equal@users.noreply.github.com>
2022-12-10 12:56:58 -0800

    Update C++ (2.1) Header.h

--- C++ (2.1) Header.h
+++ C++ (2.1) Header.h
@@ -16,7 +16,8 @@ public:
     void setX(double newX);
     void setY(double newY);
     void setFuelAmount(double newFuelAmount);
-private:
+
+protected:
     double x;
     double y;
     double fuelTank;
@@ -24,10 +25,43 @@ private:
     double fuelAmount;
 };
 
-class Bus {
+class Bus : public Car {
 public: 
-    Bus(double x_, double y_, double fuelTank_, double fuelEfficiency_, int maxPassengers_, std::vector stopNumbers_, double gasPenalty_));
+    Bus(double x_, double y_, double fuelTank_, double fuelEfficiency_, int maxPassengers_, std::vector<int> stopNumbers_, double gasPenalty_));
     Bus(const Bus & other);
 
-    std::list<> generateRoute();
+    // std::list<> generateRoute();
 };
+
+
+
+
+typedef struct Provider {
+    std::string provider;
+    std::string role;
+} Provider;
+
+typedef struct Patient {
+    std::string patient;
+    std::string condition;
+} Patient;
+
+class MedicalCenter {
+public:
+    MedicalCenter();
+    MedicalCenter(const MedicalCenter & other);
+    ~MedicalCenter();
+    std::list<Provider> getProviders();
+    std::list<Patient> getPatients();
+    void appendPatient(Patient patient_);
+    void appendProvider(Provider provider_);
+    
+
+protected:
+    std::list<Provider> providers;
+    std::list<Patient> patients;
+};
+
+
+
+class Ambulance : 
